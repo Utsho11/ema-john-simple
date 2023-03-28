99a07101d4a5483e20a87ded4eef51a5875ee6ba
Utsho11 <utshoroy521@gmail.com>
2023-03-28 20:24:34 +0600

    add to cart added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,12 +4,18 @@ import './Shop.css'
 
 const Shop = () => {
     const [products,setProduct] = useState([]);
+    const [cart,setCart] = useState([]);
 
     useEffect(() =>{
         fetch('products.json')
         .then (res => res.json())
         .then(data => setProduct(data))
     },[])
+
+    const handleAddToCart = (product)=>{
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
@@ -18,12 +24,17 @@ const Shop = () => {
                     products.map(product => <Product
                     key={product.id}
                     product={product}
+                    handleAddToCart = {handleAddToCart}
                     ></Product>)
                 }
             </div>
-            <div className='cart-container'></div>
+            <div className='cart-container'>
+                <h4>Order Summary</h4>
+                <p>Selected Items: {cart.length}</p>
+            </div>
         </div>
     );
 };
+
 
 export default Shop;
