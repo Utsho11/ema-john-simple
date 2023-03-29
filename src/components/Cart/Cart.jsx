51bc491233c5f531236ab60ef8452182fc27fc00
Utsho11 <utshoroy521@gmail.com>
2023-03-29 17:46:59 +0600

    quantity

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,10 +6,15 @@ const Cart = (props) => {
 
     let totalPrice = 0;
     let totalShipping = 0; 
+    let quantity = 0;
+
+    
     for(const product of cart){
-        console.log(product);
-        totalPrice = totalPrice + product.price;
+        product.quantity = product.quantity || 1;
+        // console.log(product);
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity * product.quantity;
     }
 
     const tax = totalPrice*7/100;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -16,12 +16,26 @@ const Shop = () => {
 
     useEffect(()=>{
         const storeCart = getShoppingCart();
+        const savedCart =[];
         // console.log(storeCart);
+        // step1: get id
         for(const id in storeCart){
-            console.log(id);
+            // console.log(id);
+            // step2: get product by using id
+        const addedProduct = products.find(product => product.id === id)
+        // console.log(addedProduct);
+        if(addedProduct){
+            // step3: get quantity of the product
+            const quantity = storeCart[id];
+            addedProduct.quantity = quantity;
+            // step4: add addedProduct to saveCart
+            savedCart.push(addedProduct)
         }
+        }
+        // step5: set the setCart
+        setCart(savedCart)
 
-    },[])
+    },[products])
 
     const handleAddToCart = (product)=>{
         const newCart = [...cart, product];
